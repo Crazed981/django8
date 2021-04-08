8442a95a64fe1326a669e8361759a9b72a2a0170
Crazed981 <test@test.ru>
2021-04-08 17:21:35 +0500

    done

--- blockapp/admin.py
+++ blockapp/admin.py
@@ -12,9 +12,9 @@ class ArticleAdminForm(forms.ModelForm):
         fields = '__all__'
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title')
+    list_display = ('id', 'title','created_at')
     list_display_links = ('id', 'title')
-    search_fields = ('title', 'content')
+    search_fields = ('title', 'content','created_at')
     form = ArticleAdminForm
 
 admin.site.register(Article, ArticleAdmin)
